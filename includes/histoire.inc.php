762c45c7ad08a9f460a71548fbbea601f5d22102
Benjamin Bourgouin <67418255+KayleoRakeon@users.noreply.github.com>
2021-03-12 01:46:10 +0100

    Partie Benji done

--- includes/histoire.inc.php
+++ includes/histoire.inc.php
@@ -11,6 +11,8 @@
         <p>Rock’N Love  est un label indépendant, créé en 2009, qui distribue ses groupes et artistes en France et à l'International, dans les réseaux professionnels de distribution, en bacs dans les magasins, en VPC et en digital sur toutes les plateformes de téléchargement légal.</p>
     </div>
 
-    <button>En savoir plus <?php include "img/picto/fleche.svg"?></button>
+    <div class="bouton">
+        <button>En savoir plus </button>
+    </div>
 
 </section>
