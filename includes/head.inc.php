6c360e34189a874c84d2ba53206ac4124fb29096
Benjamin Bourgouin <67418255+KayleoRakeon@users.noreply.github.com>
2021-03-12 11:48:12 +0100

    title

--- includes/head.inc.php
+++ includes/head.inc.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Title -->
-    <title>TITRE A CHANGER</title>
+    <title>Rock'n Love</title>
 
     <!-- Link -->
     <link rel="stylesheet" href="<?=$root?>css/style.css">
